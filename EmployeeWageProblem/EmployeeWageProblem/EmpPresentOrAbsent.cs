4a68343d3f025e20ce2c29e5f4b9271cd58cd2fa
Satish Patil <satish.patil7017@gmail.com>
2022-11-05 22:01:44 +0530

    Added Employee Present Or Absent

---
+++ EmployeeWageProblem/EmployeeWageProblem/EmpPresentOrAbsent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public static class EmployeeWage
+    {
+        public static void EmployeeCheckAttendance()
+        {
+            //define constants
+            int IS_FULL_TIME = 1;
+
+            //random function is to create random numbers
+            Random random = new Random();
+            //
+            int empCheck = random.Next(0, 2);
+            //use if else statement to check if employee is present or absent
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to EmpWageComputation problem");
+
+            //so my class is static no need to instantiate/create object of class
+            //directly call method
+
+            Console.WriteLine("To check Employee is Present or Absent as a Output:");
+            EmployeeWage.EmployeeCheckAttendance();
+        }
+    }
+}
